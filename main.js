7f3d94c266fb6cfccff7a84964ed88927e89c94d
Diksha Nigam <nigamdiksha02@gmail.com>
2023-07-09 00:59:07 +0530

    block X if 2 in a row

--- main.js
+++ main.js
@@ -1,11 +1,20 @@
 let items = document.getElementsByClassName("item");
 let itemsArray = [...items];
+let row0 = [itemsArray[0], itemsArray[1], itemsArray[2]];
+let row1 = [itemsArray[3], itemsArray[4], itemsArray[5]];
+let row2 = [itemsArray[6], itemsArray[7], itemsArray[8]];
+let col0 = [itemsArray[0], itemsArray[3], itemsArray[6]];
+let col1 = [itemsArray[1], itemsArray[4], itemsArray[7]];
+let col2 = [itemsArray[2], itemsArray[5], itemsArray[8]];
+let diag0 = [itemsArray[0], itemsArray[4], itemsArray[8]];
+let diag1 = [itemsArray[2], itemsArray[4], itemsArray[6]];
 
 function markX(selectedItem) {
   let crossMark = document.createElement("img");
   crossMark.src = "./imgs/cross.png";
   crossMark.alt = "cross-mark";
   selectedItem.append(crossMark);
+  selectedItem.dataset.markedx = "1";
 }
 
 function markO(someItem) {
@@ -13,6 +22,84 @@ function markO(someItem) {
   circleMark.src = "./imgs/circle.png";
   circleMark.alt = "circle-mark";
   someItem.append(circleMark);
+}
+
+function returnBoxWithNoX(arr) {
+  let result = -1;
+  if (!(arr[0].dataset.markedx * 1)) {
+    result = arr[0];
+  } else if (!(arr[1].dataset.markedx * 1)) {
+    result = arr[1];
+  } else {
+    result = arr[2];
+  }
+
+  return result;
+}
+
+function istwoInARow() {
+  let result = -1;
+  if (
+    itemsArray[0].dataset.markedx * 1 +
+      itemsArray[1].dataset.markedx * 1 +
+      itemsArray[2].dataset.markedx * 1 ==
+    2
+  ) {
+    result = returnBoxWithNoX(row0);
+  } else if (
+    itemsArray[3].dataset.markedx * 1 +
+      itemsArray[4].dataset.markedx * 1 +
+      itemsArray[5].dataset.markedx * 1 ==
+    2
+  ) {
+    result = returnBoxWithNoX(row1);
+  } else if (
+    itemsArray[6].dataset.markedx * 1 +
+      itemsArray[7].dataset.markedx * 1 +
+      itemsArray[8].dataset.markedx * 1 ==
+    2
+  ) {
+    result = returnBoxWithNoX(row2);
+  } else if (
+    itemsArray[0].dataset.markedx * 1 +
+      itemsArray[3].dataset.markedx * 1 +
+      itemsArray[6].dataset.markedx * 1 ==
+    2
+  ) {
+    result = returnBoxWithNoX(col0);
+  } else if (
+    itemsArray[1].dataset.markedx * 1 +
+      itemsArray[4].dataset.markedx * 1 +
+      itemsArray[7].dataset.markedx * 1 ==
+    2
+  ) {
+    result = returnBoxWithNoX(col1);
+  } else if (
+    itemsArray[2].dataset.markedx * 1 +
+      itemsArray[5].dataset.markedx * 1 +
+      itemsArray[8].dataset.markedx * 1 ==
+    2
+  ) {
+    result = returnBoxWithNoX(col2);
+  } else if (
+    itemsArray[0].dataset.markedx * 1 +
+      itemsArray[4].dataset.markedx * 1 +
+      itemsArray[8].dataset.markedx * 1 ==
+    2
+  ) {
+    result = returnBoxWithNoX(diag0);
+  } else if (
+    itemsArray[2].dataset.markedx * 1 +
+      itemsArray[4].dataset.markedx * 1 +
+      itemsArray[6].dataset.markedx * 1 ==
+    2
+  ) {
+    result = returnBoxWithNoX(diag1);
+  } else {
+    result = -1;
+  }
+
+  return result;
 }
 
 function markByComputer() {
@@ -27,9 +114,23 @@ function markByComputer() {
     return;
   }
 
-  index = Math.floor(Math.random() * itemsNotMarked.length);
-  markO(itemsNotMarked[index]);
-  itemsNotMarked[index].classList.add("markedO");
+  let index = Math.floor(Math.random() * itemsNotMarked.length);
+  let twoInARow = istwoInARow();
+  if (twoInARow == -1) {
+    markO(itemsNotMarked[index]);
+    itemsNotMarked[index].classList.add("markedO");
+  } else {
+    loc = twoInARow;
+    console.log(loc);
+    if (itemsNotMarked.indexOf(loc) != -1) {
+      markO(loc);
+      loc.classList.add("markedO");
+    } else {
+      markO(itemsNotMarked[index]);
+      itemsNotMarked[index].classList.add("markedO");
+    }
+  }
+
   let win = isWinner();
   if (win) {
     return announceWinner(win);
@@ -65,7 +166,7 @@ function isWinner() {
   ) {
     return 1;
   } else if (
-    (itemsArray[0].classList.contains("markedX") &&
+    (itemsArray[0].classList.contains("markedO") &&
       itemsArray[1].classList.contains("markedO") &&
       itemsArray[2].classList.contains("markedO")) ||
     (itemsArray[3].classList.contains("markedO") &&
@@ -118,6 +219,14 @@ itemsArray.forEach((item) => {
         return announceWinner(win);
       }
       setTimeout(markByComputer, 1000);
+      row0 = [itemsArray[0], itemsArray[1], itemsArray[2]];
+      row1 = [itemsArray[3], itemsArray[4], itemsArray[5]];
+      row2 = [itemsArray[6], itemsArray[7], itemsArray[8]];
+      col0 = [itemsArray[0], itemsArray[3], itemsArray[6]];
+      col1 = [itemsArray[1], itemsArray[4], itemsArray[7]];
+      col2 = [itemsArray[2], itemsArray[5], itemsArray[8]];
+      diag0 = [itemsArray[0], itemsArray[4], itemsArray[8]];
+      diag1 = [itemsArray[2], itemsArray[4], itemsArray[6]];
     }
   });
 });
